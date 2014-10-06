e74f6eae9578538c8b8da0e09388d851060375f2
Adam Bergen <bergen@gmail.com>
2014-10-06 14:22:52 -0500

    ook....

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
-function Time(){
+function Time() {
 
-	var today = new Date();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
 
-	if (h < 10) h = "0" + h;
-	if (m < 10) m = "0" + m;
-	if (s < 10) s = "0" + s;
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
 
-//method 2
-//var parts = ['getHours', 'getMinutes', 'getSeconds'];
-//var time2 = parts.map(function(functionName){
-//	var num = d[functionName]();
-//	return num < 10 ? "0" + num : num; 
-//});
-
-
-
-//m = checkTime(m);
-//s = checkTime(s);
-
-	document.getElementById("container").innerHTML = h + ":" + m + ":" + s;
-
-	var t = setTimeout(Time,1000);
+    //method 2
+    //var parts = ['getHours', 'getMinutes', 'getSeconds'];
+    //var time2 = parts.map(function(functionName){
+    //	var num = d[functionName]();
+    //	return num < 10 ? "0" + num : num; 
+    //});
 
 
 
-//function checkTime(i) {
-//   if (i<10) {i = "0" + i};
-//   return i;
+    //m = checkTime(m);
+    //s = checkTime(s);
+
+    document.getElementById("container").innerHTML = h + ":" + m + ":" + s;
+
+    var t = setTimeout(Time, 1000);
+
+
+
+    //function checkTime(i) {
+    //   if (i<10) {i = "0" + i};
+    //   return i;
 
 
 }
 
 
 Time();
-
