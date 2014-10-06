978933b08809a65646da3e3f9519a4c8d520f5e6
Adam Bergen <bergen@gmail.com>
2014-10-06 17:07:39 -0500

    latest push

--- js/app.js
+++ js/app.js
@@ -5,31 +5,43 @@ function Time() {
     var m = today.getMinutes();
     var s = today.getSeconds();
 
+    var time2 = h + m + s;
+
     if (h < 10) h = "0" + h;
     if (m < 10) m = "0" + m;
     if (s < 10) s = "0" + s;
 
 
-    //method 2
-    //var parts = ['getHours', 'getMinutes', 'getSeconds'];
-    //var time2 = parts.map(function(functionName){
-    //	var num = d[functionName]();
-    //	return num < 10 ? "0" + num : num; 
-    //});
-
-
-
-    //m = checkTime(m);
-    //s = checkTime(s);
-
     document.getElementById("container").innerHTML = h + ":" + m + ":" + s;
 
     var t = setTimeout(Time, 1000);
 
-    //function checkTime(i) {
-    //   if (i<10) {i = "0" + i};
-    //   return i;
 }
 
 
+//document.body.style.backgroundColor="red";
+
+
 Time();
+
+
+var x;
+
+function changeColors(){
+	x = 1;
+	setInterval(change, 1000);
+}
+
+function change(){
+	if (x === 1){
+		color = "red";
+		x = 2;
+	} else {
+		color = "green";
+		x = 1;
+	}
+
+
+document.body.style.background = color;
+
+}
