15b1cb09d517325bdc77feed2c48cac6e917e536
Adam Bergen <bergen@gmail.com>
2014-10-06 01:24:16 -0500

    dont understand my error

--- js/app.js
+++ js/app.js
@@ -5,17 +5,26 @@ function Time(){
 	var m = today.getMinutes();
 	var s = today.getSeconds();
 
-function checkTime(i) {
-    if (i<10) {i = "0" + i};
-    return i;
-}
+	if (h < 10) h = "0" + h;
+	if (m < 10) m = "0" + m;
+	if (s < 10) s = "0" + s;
 
+//m = checkTime(m);
+//s = checkTime(s);
 
-document.getElementbyId("container").innerHTML = h + ":" + m ":" + s;
+	document.getElementById("container").innerHTML = h + ":" + m + ":" + s;
 
 	var t = setTimeout(Time,1000);
 
+
+
+//function checkTime(i) {
+//   if (i<10) {i = "0" + i};
+//   return i;
+
+
 }
+
 
 Time();
 
